8c1a23f530f682e7a5c0a01cf52212aed760a517
Allan Jardine <allan@datatables.net>
2022-11-04 14:50:52 +0000

    Missing files in DataTables-DataTables

--- js/autoFill.bulma.min.mjs
+++ js/autoFill.bulma.min.mjs
@@ -1,4 +1,1 @@
-/*! Bulma integration for DataTables' AutoFill
- * ©2015 SpryMedia Ltd - datatables.net/license
- */
-import $ from"jquery";import DataTable from"datatables.net-bm";import AutoFill from"datatables.net-autofill";DataTable.AutoFill.classes.btn="button is-small";export default DataTable;
+import DataTable from"";DataTable.AutoFill.classes.btn="button is-small";export default DataTable;
