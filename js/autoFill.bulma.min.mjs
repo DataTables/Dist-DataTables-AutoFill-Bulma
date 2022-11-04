5ff0fc7494a2dc4b924531e38ae6e0216a6266a7
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 17:06:01 +0000

    d6b7ad76c39d470889acb35b9709af9865002f59
    Sync to source repo @d6b7ad76c39d470889acb35b9709af9865002f59

--- js/autoFill.bulma.min.mjs
+++ js/autoFill.bulma.min.mjs
@@ -1,1 +1,4 @@
-import DataTable from"";DataTable.AutoFill.classes.btn="button is-small";export default DataTable;
+/*! Bulma integration for DataTables' AutoFill
+ * ©2015 SpryMedia Ltd - datatables.net/license
+ */
+import $ from"jquery";import DataTable from"datatables.net-bm";import AutoFill from"datatables.net-autofill";DataTable.AutoFill.classes.btn="button is-small";export default DataTable;
