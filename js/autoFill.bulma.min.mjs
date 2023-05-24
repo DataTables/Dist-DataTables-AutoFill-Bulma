1fdaa0dda5c0964f9eee61597afe27b4b145fa6e
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-05-24 12:41:33 +0100

    5265681453c97dd9e7a21578e80239757d197278 New: Dark mode support (DataTables and Bootstrap 5 styling) New: Simplified UI - list of buttons rather than a ul/li list with inner button New: Streamlined UI look and feel Fix: `-init autoFill.alwaysAsk` wasn't working with a fill option
    8bd7a8430ead3408287b1ada56680ff5929088d9 Update: Example's code to DataTables modern style
    
    Sync to source repo @8bd7a8430ead3408287b1ada56680ff5929088d9

--- js/autoFill.bulma.min.mjs
+++ js/autoFill.bulma.min.mjs
@@ -1,4 +1,4 @@
 /*! Bulma integration for DataTables' AutoFill
  * ©2015 SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-bm";import AutoFill from"datatables.net-autofill";DataTable.AutoFill.classes.btn="button is-small";export default DataTable;
+import jQuery from"jquery";import DataTable from"datatables.net-bm";import AutoFill from"datatables.net-autofill";let $=jQuery;DataTable.AutoFill.classes.btn="button is-small";export default DataTable;
